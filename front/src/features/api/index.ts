282cb72c2ad5fbe05d65a7d6c9d7f68bd7f2934f
venus00 <farhat.oussama00@gmail.com>
2024-03-28 16:18:37 +0000

    first commit2

--- front/src/features/api/index.ts
+++ front/src/features/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const ApiClient = axios.create({
-    baseURL:'http://localhost:3001/api/v1',
+    baseURL:`http://${window.location.href}:3001/api/v1`,
     headers: {
         "Content-type": "application/json"
     }
