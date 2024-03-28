c576cf394bef6c5d1b03f4a08484f09ca28ab41c
venus00 <farhat.oussama00@gmail.com>
2024-03-28 15:56:53 +0000

    first commit

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -11,7 +11,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'dist'),
+      rootPath: join(__dirname, '..', '..', 'front', 'dist'),
     }),
     ConfigModule.forRoot({ isGlobal: true }),
     MqttModule,
